d6c6227366d9a81df815704386c3e7d1b4111080
chambrin <achambrin@outlook.fr>
2024-07-08 09:59:50 +0200

    Country list

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,30 @@
+import { useQuery, gql } from '@apollo/client';
+
+const GET_COUNTRIES = gql`
+  query GetCountries {
+    countries {
+      name
+      emoji
+    }
+  }
+`;
+
 export default function Home() {
-  return <h1>Hello, wilder !</h1>;
+  const { loading, error, data } = useQuery(GET_COUNTRIES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <div>
+      <h1>Liste des pays</h1>
+      <ul>
+        {data.countries.map(({ name, emoji }: { name: string, emoji: string }) => (
+          <li key={name}>
+            {emoji} {name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
